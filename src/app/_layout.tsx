56cb41921ddda28e6c397a1ed36038a52bcaf1d3
Elin Ternstig <elin.ternstig@gmail.com>
2026-05-06 15:35:18 +0200

    Create universal header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Stack } from 'expo-router';
+import AppHeader from '../components/appHeader'
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        header: () => <AppHeader />,
+      }} 
+    />
   );
 }
---
+++ src/components/appHeader.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AppHeader() {
+    return(
+        <View>
+            <Text>Hej hej</Text>
+        </View>
+    );
+}
